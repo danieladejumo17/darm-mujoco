c6a6f378740a0739ad1af48791f49edd0c24dcb6
Daniel <adejumodaniel17@gmail.com>
2023-03-06 12:28:58 +0100

    Updated RLlib SAC nb; Added bash scrip for training installations

---
+++ training_setup.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install build-essential -y
+pip install ray[rllib]==2.2.0 torch
+pip install wandb
+pip install tensorflow_probability
+python setup.py install
+pip install pandas -U
+sudo apt-get install libglfw3 -y
+sudo apt-get install libglfw3-dev -y
+pip install --user glfw
